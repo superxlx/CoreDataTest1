76756c275b31ca3815ee0acbe702c08f81ffe9f3
StrongX <385135499@qq.com>
2015-04-15 23:07:54 +0800

    update

---
+++ CoreDataTest1/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CoreDataTest1
+//
+//  Created by xlx on 15/4/15.
+//  Copyright (c) 2015年 xlx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var showTextView: UITextView!
+    @IBOutlet weak var addTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func addButton(sender: AnyObject) {
+    }
+    @IBAction func showButton(sender: AnyObject) {
+    }
+
+
+}
+
